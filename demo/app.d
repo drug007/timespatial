20a238d66033a48582dd9e3d1e8d7b4a68b05b8b
drug007 <drug2004@bk.ru>
2016-05-23 15:29:40 +0300

    DataWidget with visibility support

--- demo/app.d
+++ demo/app.d
@@ -1,7 +1,7 @@
 import test_gui: TestGui;
 
 import data_provider: DataProvider, TimeSpatial;
-import datawidget: DataWidget;
+import datawidget: IDataWidget, DataWidget, DataWidget2;
 import test_data: testData;
 
 struct Bar
@@ -30,23 +30,23 @@ Foo foo;
 FooBar foo_bar;
 
 
-auto makeDataWidget1()
+auto makeDataWidget1(string title)
 {
     bar = Bar(100, "some text");
     foo = Foo([bar, bar, bar, bar], "other text", size_t.min);
     foo_bar = FooBar(foo, [foo, foo, foo], "another text", int.max);
 
-    auto data_widget = new DataWidget("Widget1");
+    auto data_widget = new DataWidget(title);
     data_widget.add!FooBar(foo_bar, "header");
 
     return data_widget;
 }
 
-auto makeDataWidget2(TimeSpatial data)
+auto makeDataWidget2(string title, TimeSpatial data)
 {
     import std.conv: text;
 
-    auto data_widget = new DataWidget("Widget2");
+    auto data_widget = new DataWidget2(title);
 
     foreach(ref ds; data.dataset)
         data_widget.add(ds, ds.no.text ~ "\0");
@@ -67,10 +67,14 @@ int main(string[] args)
     int height = 768;
 
     auto data4D = new TimeSpatial(testData);
-    auto data_widget1 = makeDataWidget1();
-    auto data_widget2 = makeDataWidget2(data4D);
+    auto data_widget1 = makeDataWidget1("Widget1");
+    auto data_widget2 = makeDataWidget2("Widget2", data4D);
 
-    auto dprovider = DataProvider([data4D], [data_widget1, data_widget2]);
+    IDataWidget[] data_widget;
+    data_widget ~= data_widget1;
+    data_widget ~= data_widget2;
+
+    auto dprovider = DataProvider([data4D], data_widget);
 
     auto gui = new TestGui(width, height, dprovider);
     auto max_value = dprovider.box.max;
--- demo/datawidget.d
+++ demo/datawidget.d
@@ -83,3 +83,103 @@ class DataWidget : IDataWidget
 		add!T(value);
 	}
 }
+
+struct InfoOfV // V means visability
+{
+	IInfoOf self;
+	bool visible;
+}
+
+struct InfoOfFrameV // V means visability
+{
+	InfoOfV self;
+	InfoOfV[] child;
+}
+
+/// Создает иерархию виджетов с возможностью включения/
+/// выключения видимости данных
+class DataWidget2 : IDataWidget
+{
+	private InfoOfFrameV[] _info;
+	bool show_properties;
+	bool show_log;
+	private string _title;
+
+	this(string title)
+	{
+		show_properties = true;
+		show_log        = true;
+		_title          = title ~ "\0";
+	}
+
+	override void draw()
+	{
+		import derelict.imgui.imgui;
+
+		igSetNextWindowSize(ImVec2(400,600), ImGuiSetCond_FirstUseEver);
+		igBegin(_title.ptr, &show_log);
+		version(widget_clipping_enabled)
+		{
+			import imgui_helpers: ImGuiListClipper;
+			
+			auto clipper = ImGuiListClipper(cast(int)_info.length, igGetTextLineHeightWithSpacing());
+			size_t start = clipper.DisplayStart;
+			size_t end = clipper.DisplayEnd;
+		}
+		else
+		{
+			size_t start = 0;
+			size_t end = _info.length;
+		}
+		foreach(size_t i; start..end)
+		{
+			auto old = _info[i].self.visible;
+			igPushIdInt(cast(int) i);
+			igCheckbox("", &_info[i].self.visible);
+			igPopId();
+			igSameLine();
+
+			if(old != _info[i].self.visible)
+			{
+				import std.stdio;
+				writeln(123);
+			}
+
+			auto r = _info[i].self.self.draw();
+			if(r)
+			{
+				igIndent();
+				foreach(ref c; _info[i].child)
+					c.self.draw();
+				igUnindent();
+			}
+		}
+		version(widget_clipping_enabled) clipper.End();
+		igEnd();
+	}
+
+	auto add(T)(ref const(T) value, string header) if(is(T==struct))
+	{
+		_info ~= InfoOfFrameV(
+			InfoOfV(new InfoOf!(T)(value, header), true),
+			null,
+		);
+	}
+
+	auto add(T)(const(T) value, string header) if(is(T==struct))
+	{
+		add!T(value, header);
+	}
+
+	auto add(T)(ref const(T) value) if(is(T==struct))
+	{
+	    import std.array: back;
+
+		_info.back.child ~= InfoOfV(new InfoOf!T(value), true);
+	}
+
+	auto add(T)(const(T) value) if(is(T==struct))
+	{
+		add!T(value);
+	}
+}
