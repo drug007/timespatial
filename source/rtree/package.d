9550ce528cf09447e3e66cb83df3615d22142029
Denis Feklushkin <denis.feklushkin@gmail.com>
2016-11-01 18:24:35 +0700

    struct Point added

--- source/rtree/package.d
+++ source/rtree/package.d
@@ -16,33 +16,47 @@ class RTree
     }
 
     /// Сохраняет точку (вершину) в хранилище
-    void addPoint(long id, vec3f coords, float time, ubyte[] payload)
+    void addPoint(Point point)
     {
         Value v;
-        v.id = id,
+        v.id = point.id,
 
         // При хранении точек R*Tree BBox имеет размер 1 точка,
         // поэтому min и max совпадают
-        v.bbox.dim1.min = coords.x;
-        v.bbox.dim1.max = coords.x;
-        v.bbox.dim2.min = coords.y;
-        v.bbox.dim2.max = coords.y;
-        v.bbox.dim3.min = coords.z;
-        v.bbox.dim3.max = coords.z;
+        v.bbox.dim1.min = point.coords.x;
+        v.bbox.dim1.max = point.coords.x;
+        v.bbox.dim2.min = point.coords.y;
+        v.bbox.dim2.max = point.coords.y;
+        v.bbox.dim3.min = point.coords.z;
+        v.bbox.dim3.max = point.coords.z;
 
         // Та же ситуация и для точек во времени
-        v.bbox.dim4.min = time;
-        v.bbox.dim4.max = time;
+        v.bbox.dim4.min = point.time;
+        v.bbox.dim4.max = point.time;
 
-        v.payload = payload;
+        v.payload = point.payload;
 
         storage.addValue(v);
     }
+}
+
+struct Point
+{
+    long id;
+    vec3f coords;
+    float time;
+    ubyte[] payload;
 }
 
 unittest
 {
     auto s = new RTree("__unittest_rtree.db");
 
-    s.addPoint(100, vec3f(1,2,3), 4, [0xDE, 0xAD, 0xBE, 0xEF]);
+    Point p1;
+    p1.id = 100;
+    p1.coords = vec3f(1,2,3);
+    p1.time = 4;
+    p1.payload = [0xDE, 0xAD, 0xBE, 0xEF];
+
+    s.addPoint(p1);
 }
