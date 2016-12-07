9f6e73db8729a37f298ec840a6a331b4b555c51c
drug007 <drug2004@bk.ru>
2016-12-07 11:53:28 +0300

    WIP index added

--- source/data_index.d
+++ source/data_index.d
@@ -1,6 +1,115 @@
 module data_index;
 import color_table : ColorTable;
 
+struct Index(K, V)
+{
+    alias Key = K;
+    alias Value = V;
+    import containers.treemap: TreeMap;
+    import std.experimental.allocator.mallocator: Mallocator;
+    
+    alias Idx = TreeMap!(Key, Value, Mallocator, "a<b", false);
+    private
+    {
+        Idx idx = void;
+    }
+
+    alias idx this;
+}
+
+unittest
+{
+    import std.algorithm : map;
+    import std.typecons : AliasSeq;
+    import tests : heterogeneousData, Data;
+    import containers.dynamicarray: DynamicArray;
+
+    import std.experimental.allocator.mallocator : Mallocator;
+    import std.experimental.allocator.building_blocks : Region;
+    import std.experimental.allocator : make;
+
+    auto allocator = Region!Mallocator(1024 * 1024);
+
+    alias AllowableType = AliasSeq!(Data);
+    auto hs = heterogeneousData().map!(a=>a.value);
+    alias ByElementIndex = DynamicArray!(ulong, Mallocator, false);
+    alias ByTrackIndex = Index!(uint, ByElementIndex*);
+    alias BySourceIndex = Index!(uint, ByTrackIndex*);
+    auto idx = BySourceIndex();
+
+    size_t order_no;
+    foreach(ref e; hs)
+    {
+        import taggedalgebraic: hasType;
+
+        foreach(T; AllowableType)
+        {
+            if(e.hasType!(T))
+            {
+                ByTrackIndex* track_idx; 
+                if (!idx.containsKey(e.id.source))
+                {
+                    track_idx = allocator.make!ByTrackIndex();
+                    idx[e.id.source] = track_idx;
+                }
+                else
+                {
+                    track_idx = idx[e.id.source];
+                }
+                if(!track_idx.containsKey(e.id.no))
+                {
+                    (*track_idx)[e.id.no] = allocator.make!ByElementIndex();    
+                }
+                (*track_idx)[e.id.no].insert(order_no);
+
+                break;
+            }
+        }
+        order_no++;
+    }
+
+    import std.stdio;
+    writeln("some text");
+    foreach(ref BySourceIndex.Key k, ref BySourceIndex.Value v; idx)
+    {
+        writeln(k, ": ", v);
+        foreach(ref ByTrackIndex.Key k2, ref ByTrackIndex.Value v2; *v)
+        {
+            writeln("\t", k2, ": ", v2);
+            foreach(ref e; *v2)
+            {
+                writeln("\t\t", e);
+            }
+        }
+    }
+
+    assert(!idx.containsKey(999)); // не существует источника номер 999
+    assert(idx.containsKey(29));   // источник номер 29 существует
+
+    auto src = idx[29]; // выбираем источник номер 29
+    assert(src.length == 2);  // у источника номер 29 имеется два набора данных
+
+    assert(!src.containsKey(888)); // источник номер 29 не содержит набор данных с номером 888
+    assert(src.containsKey(1));    // источник номер 29 содержит набор данных с номером 1
+
+    // выбираем набор данных с номером 1
+    auto ds0 = (*src)[1];     // один вариант выбора набора данных с номером 1
+    auto ds = src.opIndex(1); // другой вариант выбора набора данных с номером 1
+    assert(ds0 is ds);        // оба варианты дают один и тот же результат
+    assert(ds.length == 29);  // набор данных имеет 29 элементов
+
+    size_t[] values;
+    foreach(k, v; *ds)
+        values ~= v;
+
+    import std.algorithm: equal;
+    assert(values.equal([61, 63, 65, 67, 69, 71, 73, 75, 77, 79, 81, 83, 85, 87, 89, 91, 93, 95, 97, 99, 101, 103, 105, 107, 109, 111, 113, 115, 117]));
+
+    import tests: Id;
+    assert(hs[ds.opIndex(1)].id == Id(29, 1));
+    assert(hs[ds.opIndex(1)].state == Data.State.Middle);
+}
+
 struct DataIndex(DataRange, DataObjectType)
 {
 	DataObjectType[uint][uint] idata;
