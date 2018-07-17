54dcd111d4d98b4661610325aa73a176f5ca6eec
drug007 <drug2004@bk.ru>
2018-07-17 13:09:14 +0300

    Refactoring DataItem.

--- source/data_item.d
+++ source/data_item.d
@@ -59,12 +59,6 @@ struct Attr(alias F)
     alias func = F;
 }
 
-auto makeDataItem(U, alias func)(U u)
-{
-    alias R = ReturnType!func;
-    return new DataItem!(R, kind.Converted)(u.func);
-}
-
 class DataItem(TT, alias Kind kind = Kind.Regular) : BaseDataItem
 {
     @disable
@@ -72,209 +66,39 @@ class DataItem(TT, alias Kind kind = Kind.Regular) : BaseDataItem
 
     alias T = Unqual!TT;
 
-    const(T)* ptr;
+    const(T)* data_ptr;
+    // used to convert data to textual form
+    // before passing to imgui widget for rendering
+    // because all wigdets are rendered in consequece
+    // it's safe to have one static buffer for all
+    static char[128] buffer;
+
     string header;
 
-    static if(kind == Kind.Converted)
+    this(ref const(TT) data, string header = "")
     {
-        const(T) the_copy;
-    }
+        data_ptr = &data;
 
-    static if(kind != Kind.Disabled)
-    {
-        mixin build!T;
-    }
-
-    this(ref const(T) value, string header = "")
-    {
-
-        static if(kind == Kind.Converted)
-        {
-            the_copy = value;
-            ptr = &the_copy;
-        }
+        if (header == "")
+            this.header = T.stringof ~ "\0";
         else
-        {
-            ptr = &value;
-        }
-        this.header = header;
-
-        static if(kind != Kind.Disabled)
-        {
-            mixin defineInitFunction!T;
-            initFunction();
-        }
+            this.header = header ~ "\0";
     }
 
-    this(const(T)* ptr)
+    static auto make(T, alias Kind K = Kind.Regular)(ref const(T) t, string header = "")
     {
-
-        this(*ptr);
+        return new DataItem!(T, K)(t, header);
     }
 
     override bool draw()
     {
-        static if(kind != Kind.Disabled)
-        {
-            if(header.length == 0)
-                header = T.stringof;
+        import std.format : sformat;
+        import core.exception : RangeError;
 
-            static if(isBasicType!T)
-            {
-                static if(kind == Kind.Timestamp)
-                {
-                    auto txt = (*ptr).timeToStringz;
-                }
-                else
-                {
-                    auto txt = (*ptr).text.toStringz;
-                }
-                igText(txt);
-                return false;
-            }
-            else static if(isSomeString!T)
-            {
-                igText((*ptr).toStringz);
-                return false;
-            }
-            else static if(is(T == struct) || isArray!T)
-            {
-                auto r = igTreeNodePtr(cast(void*)ptr, header.ptr, null);
-                if(r)
-                {
-                    foreach(ref e; di)
-                    {
-                        assert(e);
-                        e.draw();
-                    }
-
-                    igTreePop();
-                }
-                return r;
-            }
-            else static assert(0, "Type '" ~ T.stringof ~ "' is not supported");
-        }
-        else
-        {
+        if (data_ptr is null)
             return false;
-        }
-    }
 
-    mixin template build(T)
-    {
-        static if(is(T == struct))
-        {
-            mixin("BaseDataItem[" ~ (FieldNameTuple!T).length.text ~ "] di;");
-        }
-        else static if(isBasicType!T  || isSomeString!T)
-        {
-            mixin("BaseDataItem[] di;");
-        }        
-        else static if(isArray!T)
-        {
-            mixin("BaseDataItem[] di;");
-        }
-        else static assert(0, "Type '" ~ T.stringof ~ "' is not supported");
-    }
-
-    mixin template defineInitFunction(T)
-    {
-        static if(is(T == struct))
-        {
-            mixin template generateInitializationCode(Args...)
-            {
-                void initFunction()
-                {
-                    foreach(idx, FieldName; Args)
-                    {
-                        mixin("enum protection = __traits(getProtection, T." ~ FieldName ~ ");");
-                        static if (protection == "public")
-                        {
-                            import std.traits : PointerTarget;
-                            /// return type of field
-                            /// if field type is pointer it's pointer target type
-                            /// else it's field type itself
-                            mixin("
-                                static if(isPointer!(typeof(T." ~ FieldName ~ ")))
-                                    alias Type = Unqual!(PointerTarget!(typeof(T." ~ FieldName ~ ")));
-                                else
-                                    alias Type = Unqual!(typeof(T." ~ FieldName ~ "));
-                            ");
-                            
-                            static if(is(Type == struct)  ||
-                                        isBasicType!Type  || 
-                                        isSomeString!Type ||
-                                        isArray!Type)
-                            {
-                                import std.traits: TemplateOf, ReturnType;
-                                import std.typetuple: TypeTuple;
-                                
-                                alias ATTR = TypeTuple!(__traits(getAttributes, mixin("T." ~ FieldName)));
-                                static if(ATTR.length && __traits(compiles, __traits(isSame, TemplateOf!(ATTR[0]), Attr))) // TODO Attr ожидается только первым аргументом
-                                {
-                                    static if(__traits(isSame, TemplateOf!(ATTR[0]), Attr))
-                                    {
-                                        alias FieldType = ReturnType!(ATTR[0].func);
-                                        auto converted_value = ATTR[0].func(mixin("value." ~ FieldName));
-                                        di[idx] = new DataItem!(FieldType, Kind.Converted)(converted_value);
-                                    }
-                                }
-                                else static if(hasUDA!(mixin("T." ~ FieldName), "Disabled"))
-                                {
-                                    mixin("di[idx] = new DataItem!(Type, Kind.Disabled)(value." ~ FieldName ~ ");");
-                                }
-                                else static if(hasUDA!(mixin("T." ~ FieldName), "Timestamp"))
-                                {
-                                    mixin("di[idx] = new DataItem!(Type, Kind.Timestamp)(value." ~ FieldName ~ ");");
-                                }
-                                else static if(!isBasicType!T  && !isSomeString!T)
-                                {
-                                    static if(isArray!Type)
-                                    {
-                                        mixin("const length = value." ~ FieldName ~ ".length;");
-                                        auto header = text(Type.stringof[0..$-1] // remove closing bracket
-                                            , length                             // insert length
-                                            , "]\0");                            // add closing bracket and terminal 0
-                                        mixin("di[idx] = new DataItem!Type(value." ~ FieldName ~ ", header);");
-                                    }
-                                    else
-                                        mixin("di[idx] = new DataItem!Type(value." ~ FieldName ~ ");");
-                                }
-                            }
-                            else
-                            	pragma(msg, "Type '" ~ Type.stringof ~ "' is not supported");
-                        }
-                    }
-                }
-            }
-
-            mixin generateInitializationCode!(FieldNameTuple!T);
-        }
-        else static if(isBasicType!T)
-        {
-            // do nothing
-            void initFunction()
-            {
-            }
-        }
-        else static if(isSomeString!T)
-        {
-            // do nothing
-            void initFunction()
-            {
-            }
-        }
-        else static if(isArray!T)
-        {
-            void initFunction()
-            {
-                di.length = value.length;
-                alias Type = typeof(value[0]);
-                foreach(i; 0..di.length)
-                    di[i] = new DataItem!(Type)(value[i], text(Unqual!Type.stringof, "(", i, ")\0"));
-            }
-        }
-        else static assert(0, "Type '" ~ T.stringof ~ "' is not supported");
+        mixin (generateDraw!(T*, "data_ptr"));
     }
 }
 
@@ -328,4 +152,126 @@ unittest
     auto di = buildDataItemArray(ta);
 
     assert(di.length == ta.length);
+}
+
+/// OriginFieldType is type of a field being drawing
+/// field_name is a name of the field
+/// level is number of nesting (with 0 as the upper level)
+auto generateDraw(OriginFieldType, string field_name, int level = 0)()
+{
+    import std.traits: FieldTypeTuple, FieldNameTuple, PointerTarget;
+    import std.array : appender;
+
+    // textual representation of current level
+    enum l = level.text;
+
+    // igTreeNodePtr need the first argument to be pointer
+    // so if the field is pointer passes it directly otherwise
+    // pass a pointer to the field
+    //
+    // ptr is a pointer to the field, if the field is a pointer use it directly
+    // FieldType is a type of the pointer target 
+    static if (isPointer!OriginFieldType)
+    {
+        enum ptr =       field_name;
+        alias FieldType = Unqual!(PointerTarget!OriginFieldType);
+    }
+    else
+    {
+        enum ptr = "&" ~ field_name;
+        alias FieldType = Unqual!OriginFieldType;
+    }
+
+    auto code = appender!string;
+    code ~= "
+        {";
+
+    static if(is(FieldType == struct))
+    {
+        code ~= "
+            auto r" ~ l ~ " = igTreeNodePtr(cast(void*) " ~ ptr ~ ", " ~ l ~ " ? \"" ~ FieldType.stringof ~ "\".toStringz : header.ptr, null);
+            if(r" ~ l ~ ")
+            {";
+
+        foreach(i, fname; FieldNameTuple!FieldType)
+        {
+            enum sub_field_name = field_name ~ "." ~ fname;
+            alias SubFieldType = FieldTypeTuple!FieldType[i];
+
+            version(none)
+            {
+                // Could be used to make output more human readable but can
+                // take huge memory during compilation
+                import std.string, std.range, std.algorithm, std.conv, std.traits;
+                code ~= generateDraw!(SubFieldType, sub_field_name, level+1).splitLines.joiner("\n\t").array.to!string;
+            }
+            else
+                code ~= generateDraw!(SubFieldType, sub_field_name, level+1);
+        }
+
+        code ~= "
+
+                igTreePop();
+            }";
+    }
+    else static if(isBasicType!FieldType  || 
+                   isSomeString!FieldType ||
+                   isPointer!FieldType)
+    {
+        code ~= "
+            auto r" ~ l ~ " = false;
+            try
+            {
+                buffer.sformat(\"%s\\0\", " ~ field_name ~ ");
+            }
+            catch (RangeError re)
+            {
+                buffer.sformat(\"%s\\0\", " ~ field_name ~ ".text[0..buffer.length-1]);
+            }
+            igText(buffer.ptr);";
+    }
+    else static if(isArray!FieldType)
+    {
+        import std.range : ElementType;
+        import std.conv : to;
+
+        code ~= "
+            auto r" ~ l ~ " = igTreeNodePtr(cast(void*) " ~ ptr ~ ", " ~ l ~ " ? \"" ~ FieldType.stringof ~ "\".toStringz : header.ptr, null);
+            if(r" ~ l ~ ")
+            {
+                foreach(ref const e" ~ l ~ "; " ~ field_name ~ ")
+                {
+                    " ~ generateDraw!(Unqual!(ElementType!FieldType), "e" ~ l, level+1).to!string ~ "
+                }
+
+                igTreePop();
+            }";
+    }
+    else
+        static assert(0, "Unsupported type: " ~ FieldType.stringof);
+
+
+    // if 0 level then add return operator
+    static if (!level)
+        code ~= "
+            return r" ~ l ~ ";
+        }";
+    else
+        code ~= "
+        }";
+
+    return code.data;
+}
+
+unittest
+{
+    import tests;
+
+    auto hd = heterogeneousData();
+
+    {
+        import std.stdio;
+
+        writeln(generateDraw!(Data*, "data_ptr"));
+    }
 }
